34eb3af13d53d38434cc1a726d2da56e4ad64b02
Luxe&Kepler <168943236+78Simon87@users.noreply.github.com>
2024-08-06 22:58:58 -0500

    Add files via upload

--- scrips/about-us.js
+++ scrips/about-us.js
@@ -1,1 +1,8 @@
+const menu = document.querySelector(".btn");
+const barraLateral = document.querySelector(".navegacion");
+const desopacar = document.querySelector(".opacar");
 
+menu.addEventListener("click",()=>{
+    desopacar.classList.toggle("desopacar");
+    barraLateral.classList.toggle("barra");
+})
