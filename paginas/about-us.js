b026b4b73a7e44fae042fdb5dba05def3d58c41c
MECM <168943236+78Simon87@users.noreply.github.com>
2024-08-08 19:28:48 -0500

    Delete paginas directory

--- paginas/about-us.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector(".btn");
-const barraLateral = document.querySelector(".navegacion");
-const desopacar = document.querySelector(".opacar");
-
-menu.addEventListener("click",()=>{
-    desopacar.classList.toggle("desopacar");
-    barraLateral.classList.toggle("barra");
-})
