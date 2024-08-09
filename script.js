811de8e90d2fee08c553fa879c2ade5479b4a2f3
jhonatan castro/afdw <168943236+78Simon87@users.noreply.github.com>
2024-08-09 17:24:08 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ menu.addEventListener("click",()=>{
 const images = [
     'imagenes-herramientas/fondo-3.jpg',
     'imagenes-herramientas/fondo-imag.png',
-    'imagenes-herramientas/fondo-registro.jpg',
+    'imagenes-herramientas/',
     'imagenes-herramientas/'
 ];
 
 let currentIndex = 0;
 const slideshow = document.getElementById('slideshow');
-const interval = 5000; // Intervalo en milisegundos (5000 ms = 5 segundos)
+const interval = 6000; // Intervalo en milisegundos (5000 ms = 5 segundos)
 let autoChangeInterval;
 
 function showImage(index) {
@@ -59,3 +59,25 @@ document.getElementById('prevBtn').addEventListener('click', () => {
 // Mostrar la primera imagen al cargar
 showImage(currentIndex);
 startAutoChange(); // Inicia el cambio automático al cargar
+
+
+// Seleccionar todos los h2 dentro del contenedor .animation-text
+const headers = document.querySelectorAll('.animation-text h2');
+let textAnimation = 0; // Cambiado de currentIndex a textAnimation
+
+function showNextHeader() {
+    // Ocultar el header actual
+    headers[textAnimation].style.opacity = '0';
+
+    // Calcular el próximo índice
+    textAnimation = (textAnimation + 1) % headers.length;
+
+    // Mostrar el siguiente header
+    headers[textAnimation].style.opacity = '1';
+}
+
+// Inicializar el primer header
+headers[textAnimation].style.opacity = '1';
+
+// Cambiar el texto cada 6 segundos
+setInterval(showNextHeader, 6000); // 6000 ms = 6 s
