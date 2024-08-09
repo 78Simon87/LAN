b4f8ce48f9ca94eca569b32c3d89074f31b5db0f
MECM <168943236+78Simon87@users.noreply.github.com>
2024-08-08 19:22:03 -0500

    Add files via upload

---
+++ paginas/registro.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector(".btn");
+const barraLateral = document.querySelector(".navegacion");
+const desopacar = document.querySelector(".opacar");
+
+menu.addEventListener("click",()=>{
+    desopacar.classList.toggle("desopacar");
+    barraLateral.classList.toggle("barra");
+})
+
+const bottonAlert = document.getElementById("alert");
+// Define la función que se ejecutará cuando el botón sea clickeado
+bottonAlert.addEventListener("click", ()=>{
+    alert("Te has registrado con exito!");
+})
